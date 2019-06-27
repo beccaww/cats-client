8da96aef8e4808fdd984d0be64879332f70e7d98
Rebecca Bob <rebecca.rupel@gmail.com>
2019-06-27 15:04:01 -0700

    problem with search-page

--- src/components/search-page.js
+++ src/components/search-page.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import SearchBar from './searchbar';
-import cat from '../api/cat';
+// import React from 'react';
+// import SearchBar from './searchbar';
+// import cat from '../api/cat';
 
-class Search extends React.Component {
-    state = {
-        cats: [],
-        selectedCat: null
-    }
-    handleSubmit = async (termFromSearchBar) => {
-        const response = await cat.get('/search', {
-            params: {
-                q: termFromSearchBar
-            }
-        })
-        this.setState({
-            cats: response.data.items
-        })
-    };
-    handleCatSelect = (catImage) => {
-        this.setState({selectedCat: catImage})
-    }
+// class Search extends React.Component {
+//     state = {
+//         cats: [],
+//         selectedCat: null
+//     }
+//     handleSubmit = async (termFromSearchBar) => {
+//         const response = await cat.get('/search', {
+//             params: {
+//                 q: termFromSearchBar
+//             }
+//         })
+//         this.setState({
+//             cats: response.data.items
+//         })
+//     };
+//     handleCatSelect = (catImage) => {
+//         this.setState({selectedCat: catImage})
+//     }
 
-    render() {
-        return (
-            <div className='ui container' style={{marginTop: '1em'}}>
-                <SearchBar handleFormSubmit={this.handleSubmit}/>
-                <div className='ui grid'>
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            {/* <VideoDetail video={this.state.selectedVideo}/> */}
-                        </div>
-                        <div className="five wide column">
-                            {/* <VideoList handleVideoSelect={this.handleVideoSelect} videos={this.state.videos}/> */}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
+//     render() {
+//         return (
+//             <div className='ui container' style={{marginTop: '1em'}}>
+//                 <SearchBar handleFormSubmit={this.handleSubmit}/>
+//                 <div className='ui grid'>
+//                     <div className="ui row">
+//                         <div className="eleven wide column">
+//                             {/* <VideoDetail video={this.state.selectedVideo}/> */}
+//                         </div>
+//                         <div className="five wide column">
+//                             {/* <VideoList handleVideoSelect={this.handleVideoSelect} videos={this.state.videos}/> */}
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
-export default Search;
+// export default Search;

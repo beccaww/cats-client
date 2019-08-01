ba4d20ac42d5db696e0a92fe99aff131107e0a6f
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2019-07-31 20:19:30 -0400

    stuff

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,84 +1,101 @@
-import React, { Component } from 'react';
-import {get} from 'axios';
-import ReactGallery from 'react-photo-gallery';
-import Lightbox from 'react-images';
+import React, { Component } from "react";
+import axios from "axios";
+import ReactGallery from "react-photo-gallery";
+import Lightbox from "react-images";
+import { loadAuthToken } from "../local-storage";
 
 export default class Gallery extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            images : [],
-            currentImage: 0,
-            lightboxIsOpen: false
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      currentImage: 0,
+      lightboxIsOpen: false
+    };
+  }
 
-    componentDidMount(){
-        get('http://localhost:8080/api/images/')
-            .then(response => {
-                const images = response.data;
-                this.setState({
-                    images : images
-                })
-            })
-    }
+  componentDidMount() {
+    console.log("auth");
+    axios({
+      method: "GET",
+      url: "http://localhost:8080/api/images/",
+      headers: { authorization: `Bearer ${loadAuthToken()}` }
+    }).then(response => {
+      this.setState({
+        images: response.data
+      });
+    });
+  }
 
-    openLightbox(event, obj) {
-        this.setState({
-            currentImage: obj.index,
-            lightboxIsOpen: true,
-        });
-    }
-    closeLightbox() {
-        this.setState({
-            currentImage: 0,
-            lightboxIsOpen: false,
-        });
-    }
-    gotoPrevious() {
-        this.setState({
-            currentImage: this.state.currentImage - 1,
-        });
-    }
-    gotoNext() {
-        this.setState({
-            currentImage: this.state.currentImage + 1,
-        });
-    }
+  openLightbox(event, obj) {
+    this.setState({
+      currentImage: obj.index,
+      lightboxIsOpen: true
+    });
+  }
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false
+    });
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1
+    });
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1
+    });
+  }
 
-    render() {
-        let photos = this.state.images.map(image => {
-            return {
-                src : '/api/images' + image.uri,
-                width : image.width,
-                height : image.height,
-                id :  image.id
-            }
-        });
-        if (!this.state.images.length) return null; 
-        return (
-            <div className="gallery">
-                {this.state.images.length ?
-                    <ReactGallery
-                        photos={photos}
-                        onClick={this.openLightbox.bind(this)}
-                    />
-                    :
-                    <div className="no-images">
-                        <h5 className="text-center">
-                            You currently have no images in your photos gallery
-                        </h5>
-                    </div>
-                }
+  render() {
+    let photos = this.state.images.map(image => {
+      return {
+        src: "http://localhost:8080/api/images/" + image,
+        width: 1,
+        height: 1,
+      };
+    });
+    // const photos = [
+    //   {
+    //     src: 'http://example.com/example/img1.jpg',
+    //     width: 4,
+    //     height: 3
+    //   },
+    //   {
+    //     src: 'http://example.com/example/img2.jpg',
+    //     width: 1,
+    //     height: 1
+    //   }
+    // ];
+    console.log('photos', photos);
+    // if (!this.state.images.length) return null;
+    return (
+      <div className="gallery">
+        { this.state.images.length ? 
+          <ReactGallery
+            photos={photos}
+          />
+          :
+          <div className="no-images">
+            <h5 className="text-center">
+          
+              You currently have no images in your photos gallery
+            </h5>
+          </div>
+      }
 
-                <Lightbox images={photos}
-                          onClose={this.closeLightbox.bind(this)}
-                          onClickPrev={this.gotoPrevious.bind(this)}
-                          onClickNext={this.gotoNext.bind(this)}
-                          currentImage={this.state.currentImage}
-                          isOpen={this.state.lightboxIsOpen}/>
-            </div>
-
-        );
-    }
+        {/* <Lightbox
+          images={photos}
+          onClose={this.closeLightbox.bind(this)}
+          onClickPrev={this.gotoPrevious.bind(this)}
+          onClickNext={this.gotoNext.bind(this)}
+          currentImage={this.state.currentImage}
+          isOpen={this.state.lightboxIsOpen}
+        /> */}
+      </div>
+    );
+  }
 }
